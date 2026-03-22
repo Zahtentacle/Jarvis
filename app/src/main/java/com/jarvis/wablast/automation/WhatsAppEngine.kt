fae3cc535e9a5e93b0c06d2df1fd04b0d7f474e8
Zahtentacle <buatdownloadfilem93@gmail.com>
2026-03-22 15:17:25 +0700

    Update WhatsAppEngine.kt

--- app/src/main/java/com/jarvis/wablast/automation/WhatsAppEngine.kt
+++ app/src/main/java/com/jarvis/wablast/automation/WhatsAppEngine.kt
@@ -1,18 +1,1 @@
-package com.jarvis.wablast.automation
 
-import android.accessibilityservice.AccessibilityService
-import android.view.accessibility.AccessibilityEvent
-import android.view.accessibility.AccessibilityNodeInfo
-
-class WhatsAppEngine : AccessibilityService() {
-    override fun onAccessibilityEvent(event: AccessibilityEvent) {
-        val rootNode = rootInActiveWindow ?: return
-        val nodes = rootNode.findAccessibilityNodeInfosByViewId("com.whatsapp:id/send")
-        if (nodes.isNotEmpty()) {
-            nodes[0].performAction(AccessibilityNodeInfo.ACTION_CLICK)
-            Thread.sleep(500)
-            performGlobalAction(GLOBAL_ACTION_BACK)
-        }
-    }
-    override fun onInterrupt() {}
-}
